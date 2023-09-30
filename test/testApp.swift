adffdb5ff1d53c78e5916723694bcf58926c9265
yjian29 <120494191+yjian29@users.noreply.github.com>
2023-09-30 11:18:06 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Yinglin Jiang on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
